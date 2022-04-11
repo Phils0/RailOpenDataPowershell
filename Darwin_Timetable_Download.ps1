47d3499b0c071b0eaea603206929025276b1dfd1
Phil Sharp <phils@seatfrog.com>
2022-04-11 10:00:27 +0100

    simplify script to use single S3 download syntax

--- Darwin_Timetable_Download.ps1
+++ Darwin_Timetable_Download.ps1
@@ -1,5 +1,5 @@
 param(
-[string] $profile,              # AWS Profile with National Rail Open Data User Darwin File Access Key
+[string] $awsProfile,           # AWS Profile with National Rail Open Data User Darwin File Access Key
 [string] $outputDir = "darwin"  # Optionally a dir path to output the downloaded file to
 )
 
@@ -10,12 +10,4 @@ $bucket = "darwin.xmltimetable"
 # This is the 'directory' in S3, but called bucket in the feed information
 $keyPrefix = "PPTimetable/"    
 
-$objects = Get-S3Object -BucketName $bucket -KeyPrefix $keyPrefix -ProfileName $profile -Region $region
-
-foreach($object in $objects) {
-    $localFileName = $object.Key -replace $keyPrefix, ''
-    if ($localFileName -ne '') {
-        $localFilePath = Join-Path -Path $outputDir -ChildPath $localFileName
-        Copy-S3Object -BucketName $bucket -Key $object.Key -LocalFile $localFilePath -ProfileName $profile -Region $region
-    }
-}
+Copy-S3Object -BucketName $bucket -LocalFolder $outputDir -KeyPrefix $keyPrefix -ProfileName $awsProfile -Region $region
